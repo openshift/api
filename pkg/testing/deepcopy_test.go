30d2b80177ce5229b1e8a677e5a0783199fb8699
Maciej Szulik <maszulik@redhat.com>
2018-02-08 14:31:00 +0100

    WIP

--- pkg/testing/deepcopy_test.go
+++ pkg/testing/deepcopy_test.go
@@ -27,52 +27,31 @@ import (
 	user "github.com/openshift/api/user/v1"
 )
 
-var groups = map[schema.GroupVersion]runtime.SchemeBuilder{
-	apps.SchemeGroupVersion:          apps.SchemeBuilder,
-	authorization.SchemeGroupVersion: authorization.SchemeBuilder,
-	build.SchemeGroupVersion:         build.SchemeBuilder,
-	image.SchemeGroupVersion:         image.SchemeBuilder,
-	network.SchemeGroupVersion:       network.SchemeBuilder,
-	oauth.SchemeGroupVersion:         oauth.SchemeBuilder,
-	project.SchemeGroupVersion:       project.SchemeBuilder,
-	quota.SchemeGroupVersion:         quota.SchemeBuilder,
-	route.SchemeGroupVersion:         route.SchemeBuilder,
-	security.SchemeGroupVersion:      security.SchemeBuilder,
-	template.SchemeGroupVersion:      template.SchemeBuilder,
-	user.SchemeGroupVersion:          user.SchemeBuilder,
+var groups = []runtime.SchemeBuilder{
+	apps.SchemeBuilder,
+	authorization.SchemeBuilder,
+	build.SchemeBuilder,
+	image.SchemeBuilder,
+	network.SchemeBuilder,
+	oauth.SchemeBuilder,
+	project.SchemeBuilder,
+	quota.SchemeBuilder,
+	route.SchemeBuilder,
+	security.SchemeBuilder,
+	template.SchemeBuilder,
+	user.SchemeBuilder,
 }
 
 // TestRoundTripTypesWithoutProtobuf applies the round-trip test to all round-trippable Kinds
 // in the scheme.
 func TestRoundTripTypesWithoutProtobuf(t *testing.T) {
-	// TODO upstream this loop
-	for gv, builder := range groups {
-		t.Logf("starting group %q", gv)
-		scheme := runtime.NewScheme()
-		codecs := serializer.NewCodecFactory(scheme)
-
+	scheme := runtime.NewScheme()
+	for _, builder := range groups {
 		builder.AddToScheme(scheme)
-		seed := rand.Int63()
-		// I'm only using the generic fuzzer funcs, but at some point in time we might need to
-		// switch to specialized. For now we're happy with the current serialization test.
-		fuzzer := fuzzer.FuzzerFor(genericfuzzer.Funcs, rand.NewSource(seed), codecs)
-		kinds := scheme.KnownTypes(gv)
-
-		for kind := range kinds {
-			if globalNonRoundTrippableTypes.Has(kind) {
-				continue
-			}
-			gvk := gv.WithKind(kind)
-			// FIXME: this is explicitly testing w/o protobuf which was failing
-			// The RoundTripSpecificKindWithoutProtobuf performs the following sequence of actions:
-			// 1. object := original.DeepCopyObject()
-			// 2. obj3 := Decode(Encode(object)
-			// 3. Fuzz(object)
-			// 4. check semantic.DeepEqual(original, obj3)
-			roundtrip.RoundTripSpecificKindWithoutProtobuf(t, gvk, scheme, codecs, fuzzer, nil)
-		}
-		t.Logf("finished group %q", gv)
 	}
+	codecs := serializer.NewCodecFactory(scheme)
+	fuzzer := fuzzer.FuzzerFor(genericfuzzer.Funcs, rand.NewSource(rand.Int63()), codecs)
+	roundtrip.RoundTripExternalTypes(t, scheme, codecs, fuzzer, nil)
 }
 
 func TestFailRoundTrip(t *testing.T) {

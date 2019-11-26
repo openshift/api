3bf8cd9ddd97c7cdeb7b95ee4b9fb0ccd8e14991
Aniket <anbhat@redhat.com>
2019-11-26 08:59:49 -0500

    Update swagger docs

--- network/v1/zz_generated.swagger_doc_generated.go
+++ network/v1/zz_generated.swagger_doc_generated.go
@@ -27,9 +27,8 @@ func (ClusterNetwork) SwaggerDoc() map[string]string {
 }
 
 var map_ClusterNetworkEntry = map[string]string{
-	"":                 "ClusterNetworkEntry defines an individual cluster network. The CIDRs cannot overlap with other cluster network CIDRs, CIDRs reserved for external ips, CIDRs reserved for service networks, and CIDRs reserved for ingress ips.",
-	"CIDR":             "CIDR defines the total range of a cluster networks address space.",
-	"hostSubnetLength": "HostSubnetLength is the number of bits of the accompanying CIDR address to allocate to each node. eg, 8 would mean that each node would have a /24 slice of the overlay network for its pods.",
+	"":     "ClusterNetworkEntry defines an individual cluster network. The CIDRs cannot overlap with other cluster network CIDRs, CIDRs reserved for external ips, CIDRs reserved for service networks, and CIDRs reserved for ingress ips.",
+	"CIDR": "CIDR defines the total range of a cluster networks address space.",
 }
 
 func (ClusterNetworkEntry) SwaggerDoc() map[string]string {

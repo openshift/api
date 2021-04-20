ea944aad0431ae5c709d5f37846072d5fd7245b5
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2021-04-20 11:29:44 +0200

    APIRequestCountStatus.Conditions makes sure that tags and comments align
    otherwise genereting OpenAPI fails with: "Tags in comment and struct should match for member (Conditions) of (github.com/openshift/api/apiserver/v1.APIRequestCountStatus)"

--- apiserver/v1/types_apirequestcount.go
+++ apiserver/v1/types_apirequestcount.go
@@ -47,7 +47,7 @@ type APIRequestCountStatus struct {
 	// conditions contains details of the current status of this API Resource.
 	// +patchMergeKey=type
 	// +patchStrategy=merge
-	Conditions []metav1.Condition `json:"conditions"`
+	Conditions []metav1.Condition `json:"conditions" patchStrategy:"merge" patchMergeKey:"type"`
 
 	// removedInRelease is when the API will be removed.
 	// +kubebuilder:validation:Pattern=^[0-9][0-9]*\.[0-9][0-9]*$

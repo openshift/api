af0d2713f57087431c8fcf194442b24a7b08a17b
Brad Williams <brad.williams@redhat.com>
2021-08-12 13:51:00 -0400

    Renaming to ReleasePayload

--- release/v1alpha1/types.go
+++ release/v1alpha1/types.go
@@ -5,47 +5,56 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// PromotionTestResults aggregates the outputs of the promotion tests
+// ReleasePayload aggregates the outputs of the promotion tests
 //
 // Compatibility level 4: No compatibility is provided, the API can change at any point for any reason. These capabilities should not be used by applications needing long term support.
 // +openshift:compatibility-gen:level=4
-type PromotionTestResults struct {
+type ReleasePayload struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	// Spec the inputs used to create the promotion test
-	Spec PromotionTestSpec `json:"spec,omitempty"`
+	// Spec the inputs used to create the release payload
+	Spec ReleasePayloadSpec `json:"spec,omitempty"`
 
-	// Status is the current status of the promotion test
-	Status PromotionTestResultsStatus `json:"status,omitempty"`
+	// Status is the current status of the release payload
+	Status ReleasePayloadStatus `json:"status,omitempty"`
 }
 
-// PromotionTestSpec has the information to represent a PromotionTest
-type PromotionTestSpec struct {
+// ReleasePayloadSpec has the information to represent a PromotionTest
+type ReleasePayloadSpec struct {
 	//TODO: What are the required pieces for this...
-	Name string `json:"name,omitempty"`
 }
 
-// PromotionTestResultsStatus the status of all the promotion test jobs
-type PromotionTestResultsStatus struct {
+// ReleasePayloadStatus the status of all the promotion test jobs
+type ReleasePayloadStatus struct {
 	// BlockingJobResults stores the results of all blocking jobs
 	BlockingJobResults []JobResults `json:"blockingJobResults,omitempty"`
 	// InformingJobResults stores the results of all informing jobs
 	InformingJobResults []JobResults `json:"informingJobResults,omitempty"`
 	// AnalysisJobResults stores the results of all analysis jobs
 	AnalysisJobResults []JobResults `json:"analysisJobResults,omitempty"`
+
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
-// JobAggregateResultState the final state of the job
-type JobAggregateResultState string
+// ReleasePayloadPromotionConditionType specifies the current condition type of the ReleasePayload
+type ReleasePayloadPromotionConditionType string
 
 const (
-	// PendingJobAggregateResultState job currently running
-	PendingJobAggregateResultState JobAggregateResultState = "Pending"
-	// FailedJobAggregateResultState failed job aggregation
-	FailedJobAggregateResultState JobAggregateResultState = "Failed"
-	// SuccessJobAggregateResultState successful job aggregation
-	SuccessJobAggregateResultState JobAggregateResultState = "Success"
+        Passed ReleasePayloadPromotionConditionType = "Passed"
+		Failed ReleasePayloadPromotionConditionType = "Failed"
+)
+
+// JobState the final state of the job
+type JobState string
+
+const (
+	// PendingJobState not all job runs have completed
+	PendingJobState JobState = "Pending"
+	// FailedJobState failed job aggregation
+	FailedJobState JobState = "Failed"
+	// SuccessJobState successful job aggregation
+	SuccessJobState JobState = "Success"
 )
 
 // JobResults encapsulates the name of the job, all the results of the jobs, and an aggregated
@@ -54,23 +63,23 @@ type JobResults struct {
 	// jobName is the name of the job
 	JobName string `json:"name,omitempty"`
 	// AggregateState is the overall success/failure of all the executed jobs
-	AggregateState JobAggregateResultState `json:"state,omitempty"`
+	AggregateState JobState `json:"state,omitempty"`
 	// JobRunResults contains the links for individual jobs
 	JobRunResults []JobRunResult `json:"results,omitempty"`
 }
 
-// JobExecutionState the status of a job
-type JobExecutionState string
+// JobRunState the status of a job
+type JobRunState string
 
 const (
-	// PendingJobExecutionState job currently running
-	PendingJobExecutionState JobExecutionState = "Pending"
-	// RunningJobExecutionState job currently running
-	RunningJobExecutionState JobExecutionState = "Running"
-	// FailedJobExecutionState job failed
-	FailedJobExecutionState JobExecutionState = "Failed"
-	// SuccessJobExecutionState job successful
-	SuccessJobExecutionState JobExecutionState = "Success"
+	// PendingJobRunState job currently running
+	PendingJobRunState JobRunState = "Pending"
+	// RunningJobRunState job currently running
+	RunningJobRunState JobRunState = "Running"
+	// FailedJobRunState job failed
+	FailedJobRunState JobRunState = "Failed"
+	// SuccessJobRunState job successful
+	SuccessJobRunState JobRunState = "Success"
 )
 
 // JobRunResult the results of a job run
@@ -78,11 +87,9 @@ type JobRunResult struct {
 	// RunID the id of the job
 	RunId int `json:"runId"`
 	// State the current state of the job run
-	State JobExecutionState `json:"state"`
-	// URL the html link to the prow results
-	URL string `json:"url"`
-	// TransitionTime the timestamp of the last result change
-	TransitionTime *metav1.Time `json:"transitionTime,omitempty"`
+	State JobRunState `json:"state"`
+	// HumanProwResultsURL the html link to the prow results
+	HumanProwResultsURL string `json:"humanProwResultsURL"`
 
 	//TODO: Add field for GCS bucket
 }

96b33b94bdf26958d4c2f136039241e79da9938e
Allen Bai <abai@redhat.com>
2022-01-10 10:47:01 -0500

    helm: remove project scoped struct for status and spec
    Remove duplicated status and spec struct for namespace scoped helm chart
    repos, instead using the previous structs.
    
    Signed-off-by: Allen Bai <abai@redhat.com>

--- helm/v1beta1/types_project_helm_chart_repository.go
+++ helm/v1beta1/types_project_helm_chart_repository.go
@@ -1,7 +1,6 @@
 package v1beta1
 
 import (
-	configv1 "github.com/openshift/api/config/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -20,11 +19,11 @@ type ProjectHelmChartRepository struct {
 	// spec holds user settable values for configuration
 	// +kubebuilder:validation:Required
 	// +required
-	Spec ProjectHelmChartRepositorySpec `json:"spec"`
+	Spec HelmChartRepositorySpec `json:"spec"`
 
 	// Observed status of the repository within the namespace..
 	// +optional
-	Status ProjectHelmChartRepositoryStatus `json:"status"`
+	Status HelmChartRepositoryStatus `json:"status"`
 }
 
 // Compatibility level 2: Stable within a major release for a minimum of 9 months or 3 minor releases (whichever is longer).
@@ -35,58 +34,4 @@ type ProjectHelmChartRepositoryList struct {
 	metav1.ListMeta `json:"metadata"`
 
 	Items []ProjectHelmChartRepository `json:"items"`
-}
-
-// Helm chart repository exposed within the namespace
-type ProjectHelmChartRepositorySpec struct {
-
-	// If set to true, disable the repo usage in the namespace
-	// +optional
-	Disabled bool `json:"disabled,omitempty"`
-
-	// Optional associated human readable repository name, it can be used by UI for displaying purposes
-	// +kubebuilder:validation:MinLength=1
-	// +kubebuilder:validation:MaxLength=100
-	// +optional
-	DisplayName string `json:"name,omitempty"`
-
-	// Optional human readable repository description, it can be used by UI for displaying purposes
-	// +kubebuilder:validation:MinLength=1
-	// +kubebuilder:validation:MaxLength=2048
-	// +optional
-	Description string `json:"description,omitempty"`
-
-	// Required configuration for connecting to the chart repo
-	ProjectConnectionConfig ProjectConnectionConfig `json:"connectionConfig"`
-}
-
-type ProjectConnectionConfig struct {
-
-	// Chart repository URL
-	// +kubebuilder:validation:Pattern=`^https?:\/\/`
-	// +kubebuilder:validation:MaxLength=2048
-	URL string `json:"url"`
-
-	// ca is an optional reference to a config map by name containing the PEM-encoded CA bundle.
-	// It is used as a trust anchor to validate the TLS certificate presented by the remote server.
-	// The key "ca-bundle.crt" is used to locate the data.
-	// If empty, the default system roots are used.
-	// The namespace for this config map is openshift-config.
-	// +optional
-	CA configv1.ConfigMapNameReference `json:"ca,omitempty"`
-
-	// tlsClientConfig is an optional reference to a secret by name that contains the
-	// PEM-encoded TLS client certificate and private key to present when connecting to the server.
-	// The key "tls.crt" is used to locate the client certificate.
-	// The key "tls.key" is used to locate the private key.
-	// The namespace for this secret is openshift-config.
-	// +optional
-	TLSClientConfig configv1.SecretNameReference `json:"tlsClientConfig,omitempty"`
-}
-
-type ProjectHelmChartRepositoryStatus struct {
-
-	// conditions is a list of conditions and their statuses
-	// +optional
-	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }

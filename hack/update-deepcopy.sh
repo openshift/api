653167602396affaff37a2893e75ecf0ee54bb9d
Joel Speed <joel.speed@hotmail.co.uk>
2022-08-11 10:57:28 +0100

    Update deepcopy to use generated list of group versions
    Use the script root as the base so that it outputs to the correct
    location, even when not using a GOPATH.

--- hack/update-deepcopy.sh
+++ hack/update-deepcopy.sh
@@ -2,15 +2,17 @@
 
 source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
-SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
+
+# API_GROUP_VERSION_PACKAGES is a string of <group>/<version>.
+# The deepcopy gen needs a string of <group>:<version> so substitute slashes for colons.
+inputArg="$(echo ${API_GROUP_VERSIONS//\//:})"
 
 verify="${VERIFY:-}"
 
 GOFLAGS="" bash ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
   github.com/openshift/api/generated \
   github.com/openshift/api \
-  ${TYPE_PACKAGE_VERSIONS} \
+  "${inputArg}" \
   --go-header-file ${SCRIPT_ROOT}/hack/empty.txt \
   ${verify}
-

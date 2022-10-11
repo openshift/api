ba216349d5ce2fa534ace461773d66889e15fef8
Joel Speed <Joel.speed@hotmail.co.uk>
2022-10-11 23:15:02 +0100

    Update compatibility gen scripts

--- hack/verify-compatibility.sh
+++ hack/verify-compatibility.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
-#shellcheck source=lib/init.sh
+
 source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
-TMP_ROOT=$(mktemp --directory)
+# Build codegen-crds when it's not present and not overriden for a specific file.
+if [ -z "${CODEGEN:-}" ];then
+  ${TOOLS_MAKE} codegen
+  CODEGEN="${TOOLS_OUTPUT}/codegen"
+fi
 
-cleanup() {
-  rm -rf "${TMP_ROOT}"
-}
-trap "cleanup" EXIT SIGINT
-
-V_ROOT="${TMP_ROOT}/src/github.com/openshift/api"
-mkdir -p "$V_ROOT"
-cp -a --no-preserve=timestamp -r "$SCRIPT_ROOT"/* "$V_ROOT"
-(
-  cd "$V_ROOT" || exit
-  export GOPATH="$TMP_ROOT"
-  rm -Rf _output
-#  bash --init-file <(echo cd $V_ROOT)
-  ./hack/update-compatibility.sh > /dev/null
-)
-
-mapfile -t GENERATED < <(find "${SCRIPT_ROOT}" -name "*.go" -printf "%P\n" | sort)
-for g in "${GENERATED[@]}" ; do
-  if ! diff --unified --text "$SCRIPT_ROOT/$g" "$V_ROOT/$g" ; then
-    printf "\nopenshift_compatibility is out of date. Please run hack/update-compatibility.sh\n"
-    exit 1
-  fi
-done
+"${CODEGEN}" compatibility --base-dir "${SCRIPT_ROOT}" -v 2 --verify

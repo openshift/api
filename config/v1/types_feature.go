9ea09e15a50116a52fea924de2808985b719ee47
Jonathan Dobson <jdobson@redhat.com>
2023-02-08 16:45:24 -0700

    STOR-1077: promote CSIInlineVolumeAdmission feature gate to GA

--- config/v1/types_feature.go
+++ config/v1/types_feature.go
@@ -117,7 +117,6 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 		with("CGroupsV2").                         // sig-node, harche, OCP specific
 		with("Crun").                              // sig-node, haircommander, OCP specific
 		with("InsightsConfigAPI").                 // insights, tremes (#ccx), OCP specific
-		with("CSIInlineVolumeAdmission").          // sig-storage, jdobson, OCP specific
 		with("MatchLabelKeysInPodTopologySpread"). // sig-scheduling, ingvagabund (#forum-workloads), Kubernetes feature gate
 		with("RetroactiveDefaultStorageClass").    // sig-storage, RomanBednar, Kubernetes feature gate
 		toFeatures(),
@@ -133,7 +132,7 @@ var defaultFeatures = &FeatureGateEnabledDisabled{
 		"APIPriorityAndFairness",         // sig-apimachinery, deads2k
 		"RotateKubeletServerCertificate", // sig-pod, sjenning
 		"DownwardAPIHugePages",           // sig-node, rphillips
-		"OpenShiftPodSecurityAdmission",     // bz-auth, stlaz, OCP specific
+		"OpenShiftPodSecurityAdmission",  // bz-auth, stlaz, OCP specific
 	},
 	Disabled: []string{
 		"RetroactiveDefaultStorageClass", // sig-storage, RomanBednar, Kubernetes feature gate

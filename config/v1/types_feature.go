e42b1efa0bf5b48fdfa5ae9664a48e68e077949e
David Eads <deads@redhat.com>
2023-03-20 13:44:36 -0400

    enable CEL admission in techpreview

--- config/v1/types_feature.go
+++ config/v1/types_feature.go
@@ -119,6 +119,7 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 		with("RetroactiveDefaultStorageClass").    // sig-storage, RomanBednar, Kubernetes feature gate
 		with("PDBUnhealthyPodEvictionPolicy").     // sig-apps, atiratree (#forum-workloads), Kubernetes feature gate
 		with("DynamicResourceAllocation").         // sig-scheduling, jchaloup (#forum-workloads), Kubernetes feature gate
+		with("ValidatingAdmissionPolicy").         // sig-api-machinery, benluddy
 		toFeatures(),
 	LatencySensitive: newDefaultFeatures().
 		with(

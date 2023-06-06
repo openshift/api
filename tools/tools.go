94b679b48a258df19c40cd34ed9bcf4e3c18a15e
Jan Chaloupka <jchaloup@redhat.com>
2023-06-07 00:17:21 +0200

    prerelease-lifecycle-gen: generated deprecation of APIs

--- tools/tools.go
+++ tools/tools.go
@@ -12,5 +12,6 @@ import (
 	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/go-to-protobuf"
 	_ "k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo"
+	_ "k8s.io/code-generator/cmd/prerelease-lifecycle-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )

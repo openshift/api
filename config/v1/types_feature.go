f1f6da06da4c74009630734f671b025d0f8ba6de
Ben Luddy <bluddy@redhat.com>
2023-08-11 10:15:49 -0400

    Enable ValidatingAdmissionPolicy gate in TechPreviewNoUpgrade.

--- config/v1/types_feature.go
+++ config/v1/types_feature.go
@@ -163,6 +163,7 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 		Disabled: []FeatureGateDescription{},
 	},
 	TechPreviewNoUpgrade: newDefaultFeatures().
+		with(validatingAdmissionPolicy).
 		with(externalCloudProvider).
 		with(externalCloudProviderGCP).
 		with(csiDriverSharedResource).

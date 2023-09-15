fdc37ec0a085e6dddea0db85aa7f307c8ee7557b
David Eads <deads@redhat.com>
2023-09-15 15:11:45 -0400

    add commands for rendering and for writing featuregates

---
+++ payload-command/cmd/render/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/openshift/api/payload-command/render"
+)
+
+// this command injects the initial FeatureGate.status and places some CRDs to be created by the installer during bootstrapping
+// remember that these manifests are not maintained in a running cluster.
+func main() {
+	o := &render.RenderOpts{}
+	o.AddFlags(flag.CommandLine)
+	flag.Parse()
+
+	if err := o.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(2)
+	}
+}

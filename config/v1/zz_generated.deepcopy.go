mkowalski/openshift-api

OPNET-351: Extend infra CR to store VIPs and MachineNetwork
In order to enable work around mutable VIPs we are extending the
Infrastructure CR to hold VIPs in the spec and not only in the status.

In order to be able to perform proper validations (which currently only
happen in o/installer as VIPs are immutable) we are storing machine
networks alongside.

Implements: OPNET-351
Contributes-to: OPNET-80
Contributes-to: OPNET-340

config/v1/zz_generated.deepcopy.go
@@ -624,6 +624,21 @@ func (in *BareMetalPlatformLoadBalancer) DeepCopy() *BareMetalPlatformLoadBalanc
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *BareMetalPlatformSpec) DeepCopyInto(out *BareMetalPlatformSpec) {
 	*out = *in
+	if in.APIServerInternalIPs != nil {
+		in, out := &in.APIServerInternalIPs, &out.APIServerInternalIPs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.IngressIPs != nil {
+		in, out := &in.IngressIPs, &out.IngressIPs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	return
 }
 
@@ -654,6 +669,11 @@ func (in *BareMetalPlatformStatus) DeepCopyInto(out *BareMetalPlatformStatus) {
 		in, out := &in.LoadBalancer, &out.LoadBalancer
 		*out = new(BareMetalPlatformLoadBalancer)
 		**out = **in
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]string, len(*in))
+		copy(*out, *in)
 	}
 	return
 }
@@ -4070,6 +4090,21 @@ func (in *OpenStackPlatformLoadBalancer) DeepCopy() *OpenStackPlatformLoadBalanc
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *OpenStackPlatformSpec) DeepCopyInto(out *OpenStackPlatformSpec) {
 	*out = *in
+	if in.APIServerInternalIPs != nil {
+		in, out := &in.APIServerInternalIPs, &out.APIServerInternalIPs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.IngressIPs != nil {
+		in, out := &in.IngressIPs, &out.IngressIPs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	return
 }
 
@@ -4100,6 +4135,11 @@ func (in *OpenStackPlatformStatus) DeepCopyInto(out *OpenStackPlatformStatus) {
 		in, out := &in.LoadBalancer, &out.LoadBalancer
 		*out = new(OpenStackPlatformLoadBalancer)
 		**out = **in
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]string, len(*in))
+		copy(*out, *in)
 	}
 	return
 }
@@ -4317,12 +4357,12 @@ func (in *PlatformSpec) DeepCopyInto(out *PlatformSpec) {
 	if in.BareMetal != nil {
 		in, out := &in.BareMetal, &out.BareMetal
 		*out = new(BareMetalPlatformSpec)
-		**out = **in
+		(*in).DeepCopyInto(*out)
 	}
 	if in.OpenStack != nil {
 		in, out := &in.OpenStack, &out.OpenStack
 		*out = new(OpenStackPlatformSpec)
-		**out = **in
+		(*in).DeepCopyInto(*out)
 	}
 	if in.Ovirt != nil {
 		in, out := &in.Ovirt, &out.Ovirt
@@ -5313,6 +5353,21 @@ func (in *VSpherePlatformSpec) DeepCopyInto(out *VSpherePlatformSpec) {
 		}
 	}
 	in.NodeNetworking.DeepCopyInto(&out.NodeNetworking)
+	if in.APIServerInternalIPs != nil {
+		in, out := &in.APIServerInternalIPs, &out.APIServerInternalIPs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.IngressIPs != nil {
+		in, out := &in.IngressIPs, &out.IngressIPs
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	return
 }
 
@@ -5343,6 +5398,11 @@ func (in *VSpherePlatformStatus) DeepCopyInto(out *VSpherePlatformStatus) {
 		in, out := &in.LoadBalancer, &out.LoadBalancer
 		*out = new(VSpherePlatformLoadBalancer)
 		**out = **in
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]string, len(*in))
+		copy(*out, *in)
 	}
 	return
 }

openshift/api

config/v1/zz_generated.deepcopy.go
@@ -631,6 +631,21 @@ func (in *BareMetalPlatformLoadBalancer) DeepCopy() *BareMetalPlatformLoadBalanc
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *BareMetalPlatformSpec) DeepCopyInto(out *BareMetalPlatformSpec) {
 	*out = *in
+	if in.APIServerInternalIPs != nil {
+		in, out := &in.APIServerInternalIPs, &out.APIServerInternalIPs
+		*out = make([]IP, len(*in))
+		copy(*out, *in)
+	}
+	if in.IngressIPs != nil {
+		in, out := &in.IngressIPs, &out.IngressIPs
+		*out = make([]IP, len(*in))
+		copy(*out, *in)
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]CIDR, len(*in))
+		copy(*out, *in)
+	}
 	return
 }
 
@@ -661,6 +676,11 @@ func (in *BareMetalPlatformStatus) DeepCopyInto(out *BareMetalPlatformStatus) {
 		in, out := &in.LoadBalancer, &out.LoadBalancer
 		*out = new(BareMetalPlatformLoadBalancer)
 		**out = **in
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]CIDR, len(*in))
+		copy(*out, *in)
 	}
 	return
 }
@@ -4102,6 +4122,21 @@ func (in *OpenStackPlatformLoadBalancer) DeepCopy() *OpenStackPlatformLoadBalanc
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *OpenStackPlatformSpec) DeepCopyInto(out *OpenStackPlatformSpec) {
 	*out = *in
+	if in.APIServerInternalIPs != nil {
+		in, out := &in.APIServerInternalIPs, &out.APIServerInternalIPs
+		*out = make([]IP, len(*in))
+		copy(*out, *in)
+	}
+	if in.IngressIPs != nil {
+		in, out := &in.IngressIPs, &out.IngressIPs
+		*out = make([]IP, len(*in))
+		copy(*out, *in)
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]CIDR, len(*in))
+		copy(*out, *in)
+	}
 	return
 }
 
@@ -4132,6 +4167,11 @@ func (in *OpenStackPlatformStatus) DeepCopyInto(out *OpenStackPlatformStatus) {
 		in, out := &in.LoadBalancer, &out.LoadBalancer
 		*out = new(OpenStackPlatformLoadBalancer)
 		**out = **in
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]CIDR, len(*in))
+		copy(*out, *in)
 	}
 	return
 }
@@ -4349,12 +4389,12 @@ func (in *PlatformSpec) DeepCopyInto(out *PlatformSpec) {
 	if in.BareMetal != nil {
 		in, out := &in.BareMetal, &out.BareMetal
 		*out = new(BareMetalPlatformSpec)
-		**out = **in
+		(*in).DeepCopyInto(*out)
 	}
 	if in.OpenStack != nil {
 		in, out := &in.OpenStack, &out.OpenStack
 		*out = new(OpenStackPlatformSpec)
-		**out = **in
+		(*in).DeepCopyInto(*out)
 	}
 	if in.Ovirt != nil {
 		in, out := &in.Ovirt, &out.Ovirt
@@ -5493,6 +5533,21 @@ func (in *VSpherePlatformSpec) DeepCopyInto(out *VSpherePlatformSpec) {
 		}
 	}
 	in.NodeNetworking.DeepCopyInto(&out.NodeNetworking)
+	if in.APIServerInternalIPs != nil {
+		in, out := &in.APIServerInternalIPs, &out.APIServerInternalIPs
+		*out = make([]IP, len(*in))
+		copy(*out, *in)
+	}
+	if in.IngressIPs != nil {
+		in, out := &in.IngressIPs, &out.IngressIPs
+		*out = make([]IP, len(*in))
+		copy(*out, *in)
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]CIDR, len(*in))
+		copy(*out, *in)
+	}
 	return
 }
 
@@ -5523,6 +5578,11 @@ func (in *VSpherePlatformStatus) DeepCopyInto(out *VSpherePlatformStatus) {
 		in, out := &in.LoadBalancer, &out.LoadBalancer
 		*out = new(VSpherePlatformLoadBalancer)
 		**out = **in
+	}
+	if in.MachineNetworks != nil {
+		in, out := &in.MachineNetworks, &out.MachineNetworks
+		*out = make([]CIDR, len(*in))
+		copy(*out, *in)
 	}
 	return
 }
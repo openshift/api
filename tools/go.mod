78b782705e2a091f7b83d382fe0de8b211dfa8eb
Sam Batschelet <sbatsche@redhat.com>
2024-03-01 18:03:31 -0500

    tools: bump controller-tools

--- tools/go.mod
+++ tools/go.mod
@@ -1,32 +1,31 @@
 module github.com/openshift/api/tools
 
-go 1.20
+go 1.21
 
 require (
 	github.com/dave/dst v0.27.2
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-git/go-git/v5 v5.5.1
 	github.com/gogo/protobuf v1.3.2
-	github.com/google/go-cmp v0.5.9
+	github.com/google/go-cmp v0.6.0
 	github.com/mikefarah/yq/v4 v4.30.5
 	github.com/openshift/crd-schema-checker v0.0.0-20230918110823-6615b4c6d7c3
 	github.com/sergi/go-diff v1.1.0
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/pflag v1.0.5
 	github.com/vmware-archive/yaml-patch v0.0.11
-	golang.org/x/tools v0.8.0
-	gopkg.in/yaml.v3 v3.0.1
-	k8s.io/apiextensions-apiserver v0.28.1
-	k8s.io/apimachinery v0.28.1
-	k8s.io/code-generator v0.28.1
+	golang.org/x/tools v0.12.0
+	k8s.io/apiextensions-apiserver v0.29.0
+	k8s.io/apimachinery v0.29.0
+	k8s.io/code-generator v0.29.0
 	k8s.io/gengo v0.0.0-20230829151522-9cce18d56c01
-	k8s.io/klog/v2 v2.100.1
-	k8s.io/kube-openapi v0.0.0-20230905202853-d090da108d2f
-	sigs.k8s.io/controller-tools v0.11.1
+	k8s.io/klog/v2 v2.110.1
+	k8s.io/kube-openapi v0.0.0-20231010175941-2dd684a91f00
+	sigs.k8s.io/controller-tools v0.11.0
 	sigs.k8s.io/yaml v1.3.0
 )
 
-replace sigs.k8s.io/controller-tools => github.com/openshift/controller-tools v0.11.2-0.20230707132950-4180f051f655
+replace sigs.k8s.io/controller-tools => github.com/openshift/controller-tools v0.0.0-20240228154453-02fea9d2736b
 
 require (
 	github.com/Microsoft/go-winio v0.5.2 // indirect
@@ -37,12 +36,12 @@ require (
 	github.com/cloudflare/circl v1.1.0 // indirect
 	github.com/dimchansky/utfbom v1.1.1 // indirect
 	github.com/elliotchance/orderedmap v1.5.0 // indirect
-	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
+	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/go-git/gcfg v1.5.0 // indirect
 	github.com/go-git/go-billy/v5 v5.3.1 // indirect
-	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
@@ -70,21 +69,22 @@ require (
 	github.com/pjbgf/sha1cd v0.2.3 // indirect
 	github.com/skeema/knownhosts v1.1.0 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
-	golang.org/x/crypto v0.11.0 // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/net v0.13.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/op/go-logging.v1 v1.0.0-20160211212156-b2cb9fa56473 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/api v0.28.1 // indirect
-	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.29.0 // indirect
+	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
-	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 )
 
 // This version produces an incorrect diff, see https://github.com/sergi/go-diff/issues/123
--- tools/vendor/sigs.k8s.io/controller-tools/pkg/crd/markers/patch_validation.go
+++ tools/vendor/sigs.k8s.io/controller-tools/pkg/crd/markers/patch_validation.go
@@ -10,6 +10,9 @@ import (
 	"sigs.k8s.io/controller-tools/pkg/markers"
 )
 
+// FeatureGatesForCurrentFile is reset every
+var FeatureGatesForCurrentFile = sets.String{}
+
 var RequiredFeatureSets = sets.NewString()
 
 func init() {
@@ -26,6 +29,9 @@ func init() {
 const OpenShiftFeatureSetMarkerName = "openshift:enable:FeatureSets"
 const OpenShiftFeatureSetAwareEnumMarkerName = "openshift:validation:FeatureSetAwareEnum"
 const OpenShiftFeatureSetAwareXValidationMarkerName = "openshift:validation:FeatureSetAwareXValidation"
+const OpenShiftFeatureGateMarkerName = "openshift:enable:FeatureGate"
+const OpenShiftFeatureGateAwareEnumMarkerName = "openshift:validation:FeatureGateAwareEnum"
+const OpenShiftFeatureGateAwareXValidationMarkerName = "openshift:validation:FeatureGateAwareXValidation"
 
 func init() {
 	ValidationMarkers = append(ValidationMarkers,
@@ -39,6 +45,19 @@ func init() {
 	ValidationMarkers = append(ValidationMarkers,
 		must(markers.MakeDefinition(OpenShiftFeatureSetAwareXValidationMarkerName, markers.DescribesType, FeatureSetXValidation{})).
 			WithHelp(markers.SimpleHelp("OpenShift", "specifies the FeatureSet that is required to generate this XValidation rule.")),
+	)
+
+	ValidationMarkers = append(ValidationMarkers,
+		must(markers.MakeDefinition(OpenShiftFeatureGateAwareEnumMarkerName, markers.DescribesField, FeatureGateEnum{})).
+			WithHelp(markers.SimpleHelp("OpenShift", "specifies the FeatureGate that is required to generate this field.")),
+	)
+	FieldOnlyMarkers = append(FieldOnlyMarkers,
+		must(markers.MakeDefinition(OpenShiftFeatureGateMarkerName, markers.DescribesField, []string{})).
+			WithHelp(markers.SimpleHelp("OpenShift", "specifies the FeatureGate that is required to generate this field.")),
+	)
+	ValidationMarkers = append(ValidationMarkers,
+		must(markers.MakeDefinition(OpenShiftFeatureGateAwareXValidationMarkerName, markers.DescribesType, FeatureGateXValidation{})).
+			WithHelp(markers.SimpleHelp("OpenShift", "specifies the FeatureGate that is required to generate this XValidation rule.")),
 	)
 }
 
@@ -95,3 +114,57 @@ func (m FeatureSetXValidation) ApplyToSchema(schema *apiext.JSONSchemaProps) err
 // that the FeatureSetXValidation is applied before the XValidation so that
 // the order is stable.
 func (m FeatureSetXValidation) ApplyFirst() {}
+
+type FeatureGateEnum struct {
+	FeatureGateNames []string `marker:"featureGate"`
+	EnumValues       []string `marker:"enum"`
+}
+
+func (m FeatureGateEnum) ApplyToSchema(schema *apiext.JSONSchemaProps) error {
+	if !FeatureGatesForCurrentFile.HasAny(m.FeatureGateNames...) {
+		return nil
+	}
+
+	// TODO(directxman12): this is a bit hacky -- we should
+	// probably support AnyType better + using the schema structure
+	vals := make([]apiext.JSON, len(m.EnumValues))
+	for i, val := range m.EnumValues {
+		// TODO(directxman12): check actual type with schema type?
+		// if we're expecting a string, marshal the string properly...
+		// NB(directxman12): we use json.Marshal to ensure we handle JSON escaping properly
+		valMarshalled, err := json.Marshal(val)
+		if err != nil {
+			return err
+		}
+		vals[i] = apiext.JSON{Raw: valMarshalled}
+	}
+
+	schema.Enum = vals
+	return nil
+}
+
+type FeatureGateXValidation struct {
+	FeatureGateNames []string `marker:"featureGate"`
+	Rule             string
+	Message          string `marker:",optional"`
+}
+
+func (m FeatureGateXValidation) ApplyToSchema(schema *apiext.JSONSchemaProps) error {
+	if !FeatureGatesForCurrentFile.HasAny(m.FeatureGateNames...) {
+		return nil
+	}
+
+	validation := XValidation{
+		Rule:    m.Rule,
+		Message: m.Message,
+	}
+
+	return validation.ApplyToSchema(schema)
+}
+
+// ApplyFirst means that this will be applied in the first run of markers.
+// We do this because, when validations are applied, the markers come out of
+// a map which means that the order is not guaranteed. We want to make sure
+// that the FeatureGateXValidation is applied before the XValidation so that
+// the order is stable.
+func (m FeatureGateXValidation) ApplyFirst() {}

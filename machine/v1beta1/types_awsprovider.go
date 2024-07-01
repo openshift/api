5b497683252ed357f2cd9194c3019c5b8fe01cad
chiragkyal <ckyal@redhat.com>
2024-07-01 17:06:37 +0530

    CFE-1063: remove kubebuilder validation for placementGroupPartition
    Signed-off-by: chiragkyal <ckyal@redhat.com>

--- machine/v1beta1/types_awsprovider.go
+++ machine/v1beta1/types_awsprovider.go
@@ -87,8 +87,6 @@ type AWSMachineProviderConfig struct {
 	// placementGroupPartition is the partition number within the placement group in which to launch the instance.
 	// This must be an integer value between 1 and 7. It is only valid if the placement group, referred in
 	// `PlacementGroupName` was created with strategy set to partition.
-	// +kubebuilder:validation:Minimum:=1
-	// +kubebuilder:validation:Maximum:=7
 	// +optional
 	PlacementGroupPartition int32 `json:"placementGroupPartition,omitempty"`
 }

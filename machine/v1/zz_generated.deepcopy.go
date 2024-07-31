nutanix-cloud-native/openshift-api

Nutanix mapi: gpus and dataDisks support

machine/v1/zz_generated.deepcopy.go
@@ -6,6 +6,7 @@
 package v1
 
 import (
+	configv1 "github.com/openshift/api/config/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
@@ -609,6 +610,22 @@ func (in *NutanixCategory) DeepCopy() *NutanixCategory {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NutanixDiskAddress) DeepCopyInto(out *NutanixDiskAddress) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NutanixDiskAddress.
+func (in *NutanixDiskAddress) DeepCopy() *NutanixDiskAddress {
+	if in == nil {
+		return nil
+	}
+	out := new(NutanixDiskAddress)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *NutanixFailureDomainReference) DeepCopyInto(out *NutanixFailureDomainReference) {
 	*out = *in
 	return
@@ -620,6 +637,32 @@ func (in *NutanixFailureDomainReference) DeepCopy() *NutanixFailureDomainReferen
 		return nil
 	}
 	out := new(NutanixFailureDomainReference)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NutanixGPU) DeepCopyInto(out *NutanixGPU) {
+	*out = *in
+	if in.DeviceID != nil {
+		in, out := &in.DeviceID, &out.DeviceID
+		*out = new(int64)
+		**out = **in
+	}
+	if in.Name != nil {
+		in, out := &in.Name, &out.Name
+		*out = new(string)
+		**out = **in
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NutanixGPU.
+func (in *NutanixGPU) DeepCopy() *NutanixGPU {
+	if in == nil {
+		return nil
+	}
+	out := new(NutanixGPU)
 	in.DeepCopyInto(out)
 	return out
 }
@@ -645,6 +688,20 @@ func (in *NutanixMachineProviderConfig) DeepCopyInto(out *NutanixMachineProvider
 		in, out := &in.Categories, &out.Categories
 		*out = make([]NutanixCategory, len(*in))
 		copy(*out, *in)
+	}
+	if in.GPUs != nil {
+		in, out := &in.GPUs, &out.GPUs
+		*out = make([]NutanixGPU, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	if in.DataDisks != nil {
+		in, out := &in.DataDisks, &out.DataDisks
+		*out = make([]NutanixVMDisk, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
 	}
 	if in.UserDataSecret != nil {
 		in, out := &in.UserDataSecret, &out.UserDataSecret
@@ -741,6 +798,112 @@ func (in *NutanixResourceIdentifier) DeepCopy() *NutanixResourceIdentifier {
 		return nil
 	}
 	out := new(NutanixResourceIdentifier)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NutanixStorageContainerReference) DeepCopyInto(out *NutanixStorageContainerReference) {
+	*out = *in
+	if in.UUID != nil {
+		in, out := &in.UUID, &out.UUID
+		*out = new(string)
+		**out = **in
+	}
+	if in.Name != nil {
+		in, out := &in.Name, &out.Name
+		*out = new(string)
+		**out = **in
+	}
+	if in.URL != nil {
+		in, out := &in.URL, &out.URL
+		*out = new(string)
+		**out = **in
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NutanixStorageContainerReference.
+func (in *NutanixStorageContainerReference) DeepCopy() *NutanixStorageContainerReference {
+	if in == nil {
+		return nil
+	}
+	out := new(NutanixStorageContainerReference)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NutanixVMDisk) DeepCopyInto(out *NutanixVMDisk) {
+	*out = *in
+	out.DiskSize = in.DiskSize.DeepCopy()
+	if in.DeviceProperties != nil {
+		in, out := &in.DeviceProperties, &out.DeviceProperties
+		*out = new(NutanixVMDiskDeviceProperties)
+		**out = **in
+	}
+	if in.StorageConfig != nil {
+		in, out := &in.StorageConfig, &out.StorageConfig
+		*out = new(NutanixVMStorageConfig)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.VolumeGroup != nil {
+		in, out := &in.VolumeGroup, &out.VolumeGroup
+		*out = new(configv1.NutanixResourceIdentifier)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.DataSource != nil {
+		in, out := &in.DataSource, &out.DataSource
+		*out = new(configv1.NutanixResourceIdentifier)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NutanixVMDisk.
+func (in *NutanixVMDisk) DeepCopy() *NutanixVMDisk {
+	if in == nil {
+		return nil
+	}
+	out := new(NutanixVMDisk)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NutanixVMDiskDeviceProperties) DeepCopyInto(out *NutanixVMDiskDeviceProperties) {
+	*out = *in
+	out.DiskAddress = in.DiskAddress
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NutanixVMDiskDeviceProperties.
+func (in *NutanixVMDiskDeviceProperties) DeepCopy() *NutanixVMDiskDeviceProperties {
+	if in == nil {
+		return nil
+	}
+	out := new(NutanixVMDiskDeviceProperties)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NutanixVMStorageConfig) DeepCopyInto(out *NutanixVMStorageConfig) {
+	*out = *in
+	if in.StorageContainer != nil {
+		in, out := &in.StorageContainer, &out.StorageContainer
+		*out = new(NutanixStorageContainerReference)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NutanixVMStorageConfig.
+func (in *NutanixVMStorageConfig) DeepCopy() *NutanixVMStorageConfig {
+	if in == nil {
+		return nil
+	}
+	out := new(NutanixVMStorageConfig)
 	in.DeepCopyInto(out)
 	return out
 }

openshift/api

machine/v1/zz_generated.deepcopy.go
@@ -625,6 +625,32 @@ func (in *NutanixFailureDomainReference) DeepCopy() *NutanixFailureDomainReferen
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NutanixGPU) DeepCopyInto(out *NutanixGPU) {
+	*out = *in
+	if in.DeviceID != nil {
+		in, out := &in.DeviceID, &out.DeviceID
+		*out = new(int32)
+		**out = **in
+	}
+	if in.Name != nil {
+		in, out := &in.Name, &out.Name
+		*out = new(string)
+		**out = **in
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NutanixGPU.
+func (in *NutanixGPU) DeepCopy() *NutanixGPU {
+	if in == nil {
+		return nil
+	}
+	out := new(NutanixGPU)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *NutanixMachineProviderConfig) DeepCopyInto(out *NutanixMachineProviderConfig) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
@@ -645,6 +671,20 @@ func (in *NutanixMachineProviderConfig) DeepCopyInto(out *NutanixMachineProvider
 		in, out := &in.Categories, &out.Categories
 		*out = make([]NutanixCategory, len(*in))
 		copy(*out, *in)
+	}
+	if in.GPUs != nil {
+		in, out := &in.GPUs, &out.GPUs
+		*out = make([]NutanixGPU, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	if in.DataDisks != nil {
+		in, out := &in.DataDisks, &out.DataDisks
+		*out = make([]NutanixVMDisk, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
 	}
 	if in.UserDataSecret != nil {
 		in, out := &in.UserDataSecret, &out.UserDataSecret
@@ -741,6 +781,96 @@ func (in *NutanixResourceIdentifier) DeepCopy() *NutanixResourceIdentifier {
 		return nil
 	}
 	out := new(NutanixResourceIdentifier)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NutanixStorageResourceIdentifier) DeepCopyInto(out *NutanixStorageResourceIdentifier) {
+	*out = *in
+	if in.UUID != nil {
+		in, out := &in.UUID, &out.UUID
+		*out = new(string)
+		**out = **in
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NutanixStorageResourceIdentifier.
+func (in *NutanixStorageResourceIdentifier) DeepCopy() *NutanixStorageResourceIdentifier {
+	if in == nil {
+		return nil
+	}
+	out := new(NutanixStorageResourceIdentifier)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NutanixVMDisk) DeepCopyInto(out *NutanixVMDisk) {
+	*out = *in
+	out.DiskSize = in.DiskSize.DeepCopy()
+	if in.DeviceProperties != nil {
+		in, out := &in.DeviceProperties, &out.DeviceProperties
+		*out = new(NutanixVMDiskDeviceProperties)
+		**out = **in
+	}
+	if in.StorageConfig != nil {
+		in, out := &in.StorageConfig, &out.StorageConfig
+		*out = new(NutanixVMStorageConfig)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.DataSource != nil {
+		in, out := &in.DataSource, &out.DataSource
+		*out = new(NutanixResourceIdentifier)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NutanixVMDisk.
+func (in *NutanixVMDisk) DeepCopy() *NutanixVMDisk {
+	if in == nil {
+		return nil
+	}
+	out := new(NutanixVMDisk)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NutanixVMDiskDeviceProperties) DeepCopyInto(out *NutanixVMDiskDeviceProperties) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NutanixVMDiskDeviceProperties.
+func (in *NutanixVMDiskDeviceProperties) DeepCopy() *NutanixVMDiskDeviceProperties {
+	if in == nil {
+		return nil
+	}
+	out := new(NutanixVMDiskDeviceProperties)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NutanixVMStorageConfig) DeepCopyInto(out *NutanixVMStorageConfig) {
+	*out = *in
+	if in.StorageContainer != nil {
+		in, out := &in.StorageContainer, &out.StorageContainer
+		*out = new(NutanixStorageResourceIdentifier)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NutanixVMStorageConfig.
+func (in *NutanixVMStorageConfig) DeepCopy() *NutanixVMStorageConfig {
+	if in == nil {
+		return nil
+	}
+	out := new(NutanixVMStorageConfig)
 	in.DeepCopyInto(out)
 	return out
 }
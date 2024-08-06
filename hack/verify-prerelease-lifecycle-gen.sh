86a592fde8936fb6f43b2b61ce64a5bd31932897
Filip Křepinský <atiratree@gmail.com>
2024-08-06 16:49:38 +0200

    fix verify-prerelease-lifecycle-gen.sh
    --verify-only option has been removed, so use git diff instead

--- hack/verify-prerelease-lifecycle-gen.sh
+++ hack/verify-prerelease-lifecycle-gen.sh
@@ -2,4 +2,12 @@
 
 source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
-EXTRA_ARGS=--verify-only ${SCRIPT_ROOT}/hack/update-prerelease-lifecycle-gen.sh
+${SCRIPT_ROOT}/hack/update-prerelease-lifecycle-gen.sh
+
+ret=0
+git diff --exit-code --quiet || ret=$?
+if [[ $ret -ne 0 ]]; then
+  echo "Prerelease lifecycle generation is out of date. Please run hack/update-prerelease-lifecycle-gen.sh"
+  exit 1
+fi
+echo "Prerelease lifecycle generation up to date."

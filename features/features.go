8f11151311c790debcb0a5bab97efec554db9328
Sandhya Dasu <sdasu@redhat.com>
2024-08-06 13:46:24 -0400

    Promote Azure CAPI as the default Install method

--- features/features.go
+++ features/features.go
@@ -493,7 +493,7 @@ var (
 						reportProblemsToJiraComponent("Installer").
 						contactPerson("jhixson74").
 						productScope(ocpSpecific).
-						enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
+						enableIn(configv1.Default, configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
 						mustRegister()
 
 	FeatureGateClusterAPIInstallGCP = newFeatureGate("ClusterAPIInstallGCP").

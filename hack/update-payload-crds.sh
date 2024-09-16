fd78eea5bb1244eeead2be03a9ff896e51cf25e6
David Eads <deads@redhat.com>
2024-09-16 15:33:24 -0400

    move authoritative source of some operator CRD manifests to openshift/api

--- hack/update-payload-crds.sh
+++ hack/update-payload-crds.sh
@@ -11,6 +11,16 @@ crd_globs="\
     quota/v1/zz_generated.crd-manifests/*_config-operator_*.crd*yaml\
     security/v1/zz_generated.crd-manifests/*_config-operator_*.crd*yaml\
     securityinternal/v1/zz_generated.crd-manifests/*_config-operator_*.crd*yaml
+    operator/v1/zz_generated.crd-manifests/0000_50_authentication_01_authentications*.crd.yaml
+    operator/v1/zz_generated.crd-manifests/0000_30_openshift-apiserver_01_openshiftapiservers*.crd.yaml
+    operator/v1/zz_generated.crd-manifests/0000_20_kube-apiserver_01_kubeapiservers*.crd.yaml
+    operator/v1/zz_generated.crd-manifests/0000_12_etcd_01_etcds*.crd.yaml
+    operator/v1alpha1/zz_generated.crd-manifests/0000_10_etcd_01_etcdbackups*.crd.yaml
+    config/v1alpha1/zz_generated.crd-manifests/0000_10_config-operator_01_backups*.crd.yaml
+    operator/v1/zz_generated.crd-manifests/0000_25_kube-scheduler_01_kubeschedulers*.crd.yaml
+    operator/v1/zz_generated.crd-manifests/0000_25_kube-controller-manager_01_kubecontrollermanagers*.crd.yaml
+    config/v1/zz_generated.crd-manifests/0000_10_openshift-controller-manager_01_builds*.crd.yaml
+    operator/v1/zz_generated.crd-manifests/0000_50_openshift-controller-manager_02_openshiftcontrollermanagers*.crd.yaml
     "
 
 # To allow the crd_globs to be sourced in the verify script,

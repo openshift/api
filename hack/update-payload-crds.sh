873d8c8d3cab4dc3a2722b2c66c02efd430fa3c2
David Eads <deads@redhat.com>
2024-09-17 16:51:19 -0400

    copy mco crds to be managed by openshfit/api

--- hack/update-payload-crds.sh
+++ hack/update-payload-crds.sh
@@ -21,6 +21,10 @@ crd_globs="\
     operator/v1/zz_generated.crd-manifests/0000_25_kube-controller-manager_01_kubecontrollermanagers*.crd.yaml
     config/v1/zz_generated.crd-manifests/0000_10_openshift-controller-manager_01_builds*.crd.yaml
     operator/v1/zz_generated.crd-manifests/0000_50_openshift-controller-manager_02_openshiftcontrollermanagers*.crd.yaml
+    machineconfiguration/v1/zz_generated.crd-manifests/*.crd.yaml
+    machineconfiguration/v1alpha1/zz_generated.crd-manifests/*.crd.yaml
+    operator/v1/zz_generated.crd-manifests/0000_80_machine-config_01_machineconfigurations*.crd.yaml
+    config/v1alpha1/zz_generated.crd-manifests/0000_10_config-operator_01_clusterimagepolicies*.crd.yaml
     "
 
 # To allow the crd_globs to be sourced in the verify script,

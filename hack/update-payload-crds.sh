c51a459840f78808747bf37b78a66624fe7e87b6
Joel Speed <joel.speed@hotmail.co.uk>
2025-01-16 18:03:07 +0000

    Monitoring Config should start life as v1alpha1

--- hack/update-payload-crds.sh
+++ hack/update-payload-crds.sh
@@ -31,6 +31,7 @@ crd_globs="\
     operator/v1/zz_generated.crd-manifests/0000_80_machine-config_01_machineconfigurations*.crd.yaml
     config/v1alpha1/zz_generated.crd-manifests/0000_10_config-operator_01_clusterimagepolicies*.crd.yaml
     config/v1alpha1/zz_generated.crd-manifests/0000_10_config-operator_01_imagepolicies*.crd.yaml
+    config/v1alpha1/zz_generated.crd-manifests/0000_10_config-operator_01_clustermonitoring*.crd.yaml
     operator/v1/zz_generated.crd-manifests/*_storage_01_storages*.crd.yaml
     operator/v1/zz_generated.crd-manifests/*_csi-driver_01_clustercsidrivers*.crd.yaml
     "

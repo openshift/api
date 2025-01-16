d6b81cdb55d78140f2d9e5e388817af020af6a08
vr4manta <vr4manta@gmail.com>
2025-01-16 08:09:17 -0500

    Added thinProvisioned flag for data disks

--- machine/v1beta1/types_vsphereprovider.go
+++ machine/v1beta1/types_vsphereprovider.go
@@ -193,11 +193,17 @@ type VSphereDisk struct {
 	// +required
 	Name string `json:"name"`
 	// sizeGiB is the size of the disk in GiB.
-	// The maximum supported size is 57742 GiB.
+	// The maximum supported size is 16384 GiB.
 	// +kubebuilder:validation:Minimum=1
-	// +kubebuilder:validation:Maximum=57742
+	// +kubebuilder:validation:Maximum=16384
 	// +required
 	SizeGiB int32 `json:"sizeGiB"`
+	// thinProvisioned flag to indicate whether the virtual disk backing
+	// file should be allocated lazily (using thin provisioning) or be
+	// fully allocated (standard provisioning).
+	// If not set, the setting will be provided by the default storage policy.
+	// +optional
+	ThinProvisioned bool `json:"thinProvisioned,omitempty"`
 }
 
 // WorkspaceConfig defines a workspace configuration for the vSphere cloud
--- openapi/generated_openapi/zz_generated.openapi.go
+++ openapi/generated_openapi/zz_generated.openapi.go
@@ -38951,10 +38951,17 @@ func schema_openshift_api_machine_v1beta1_VSphereDisk(ref common.ReferenceCallba
 					},
 					"sizeGiB": {
 						SchemaProps: spec.SchemaProps{
-							Description: "sizeGiB is the size of the disk in GiB. The maximum supported size is 57742 GiB.",
+							Description: "sizeGiB is the size of the disk in GiB. The maximum supported size is 16384 GiB.",
 							Default:     0,
 							Type:        []string{"integer"},
 							Format:      "int32",
+						},
+					},
+					"thinProvisioned": {
+						SchemaProps: spec.SchemaProps{
+							Description: "thinProvisioned flag to indicate whether the virtual disk backing file should be allocated lazily (using thin provisioning) or be fully allocated (standard provisioning). If not set, the setting will be provided by the default storage policy.",
+							Type:        []string{"boolean"},
+							Format:      "",
 						},
 					},
 				},

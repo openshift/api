47f1350911af2440d6f68de5a608dd635f4b870e
Petr Muller <muller@redhat.com>
2025-02-04 18:28:20 +0100

    status: remove `kubebuilder:validation:Optional` from doc.go

--- update/v1alpha1/doc.go
+++ update/v1alpha1/doc.go
@@ -2,7 +2,6 @@
 // +k8s:defaulter-gen=TypeMeta
 // +k8s:openapi-gen=true
 
-// +kubebuilder:validation:Optional
 // +groupName=update.openshift.io
 // Package v1alpha1 is the v1alpha1 version of the API.
 package v1alpha1

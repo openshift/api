d21f519af828cbe953f624f4cf57d350c4df349a
Petr Muller <muller@redhat.com>
2025-03-18 18:36:37 +0100

    update: enforce known reasons on COs

--- update/v1alpha1/types_clusteroperator_progress_insight.go
+++ update/v1alpha1/types_clusteroperator_progress_insight.go
@@ -53,7 +53,11 @@ type ClusterOperatorProgressInsightStatus struct {
 	// +patchStrategy=merge
 	// +patchMergeKey=type
 	// +optional
-	// +kubebuilder:validation:MaxItems=10
+	// +kubebuilder:validation:MinItems=2
+	// +kubebuilder:validation:MaxItems=2
+	// +kubebuilder:validation:XValidation:rule="self.all(c, c.type == 'Updating' || c.type == 'Healthy')",message="Cluster Operator Progress Insight conditions must have 'Updating' and 'Healthy' conditions"
+	// +kubebuilder:validation:XValidation:rule="self.all(c, c.type != 'Updating' || c.type == 'Healthy' && (c.reason == 'AsExpected' || c.reason == 'Unavailable' || c.reason == 'Degraded' || c.reason == 'CannotDetermine'))",message="Cluster Operator Progress Insight Healthy condition must have one of known reasons: 'AsExpected', 'Unavailable', 'Degraded', or 'CannotDetermine'"
+	// +kubebuilder:validation:XValidation:rule="self.all(c, c.type != 'Healthy' || c.type == 'Updating' && (c.reason == 'Updated' || c.reason == 'Pending' || c.reason == 'Progressing' || c.reason == 'CannotDetermine'))",message="Cluster Operator Progress Insight Updating condition must have one of known reasons: 'Updated', 'Pending', 'Progressing', or 'CannotDetermine'"
 	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
 
 	// name is the name of the operator

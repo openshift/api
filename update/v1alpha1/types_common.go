e1bfa53bf97b195c3be61a774e62d70812fb0b70
Petr Muller <muller@redhat.com>
2025-03-19 16:54:26 +0100

    update: move `ResourceRef to types_common.go

--- update/v1alpha1/types_common.go
+++ update/v1alpha1/types_common.go
@@ -1,5 +1,35 @@
 package v1alpha1
 
+// ResourceRef is a reference to a kubernetes resource, typically involved in an insight
+type ResourceRef struct {
+	// group of the object being referenced, if any
+	// +optional
+	// +kubebuilder:validation:MaxLength=253
+	// +kubebuilder:validation:XValidation:rule="!format.dns1123Subdomain().validate(self).hasValue()",message="a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character."
+	Group string `json:"group,omitempty"`
+
+	// resource of object being referenced
+	// +required
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MaxLength=253
+	// +kubebuilder:validation:XValidation:rule="!format.dns1123Subdomain().validate(self).hasValue()",message="a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character."
+	Resource string `json:"resource"`
+
+	// name of the object being referenced
+	// +required
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MaxLength=253
+	// +kubebuilder:validation:XValidation:rule="!format.dns1123Subdomain().validate(self).hasValue()",message="a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character."
+	Name string `json:"name"`
+
+	// namespace of the object being referenced, if any
+	// +optional
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MaxLength=253
+	// +kubebuilder:validation:Pattern=`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`
+	Namespace string `json:"namespace,omitempty"`
+}
+
 // ScopeType is one of ControlPlane or WorkerPool
 // +kubebuilder:validation:Enum=ControlPlane;WorkerPool
 type ScopeType string

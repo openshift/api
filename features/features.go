a2cb505d8099537ef58cabc0f48a670d3845e3f3
Bryce Palmer <bpalmer@redhat.com>
2025-04-11 13:17:36 -0400

    features: promote ExternalOIDCWithUIDAndExtraClaimMappings to GA for Hypershift
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

--- features/features.go
+++ features/features.go
@@ -504,7 +504,7 @@ var (
 				productScope(ocpSpecific).
 				enhancementPR("https://github.com/openshift/enhancements/pull/1596").
 				enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
-				enableForClusterProfile(Hypershift, configv1.TechPreviewNoUpgrade).
+				enableForClusterProfile(Hypershift, configv1.TechPreviewNoUpgrade, configv1.Default).
 				mustRegister()
 
 	FeatureGateExample = newFeatureGate("Example").

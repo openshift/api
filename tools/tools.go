4730f2eecdc54056ea412356f1b8e6830c4dea62
Joel Speed <joel.speed@hotmail.co.uk>
2025-05-14 14:01:54 +0100

    Update configuration to move to golangci-lint v2

--- tools/tools.go
+++ tools/tools.go
@@ -7,7 +7,7 @@ import (
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/gogo/protobuf/proto"
 	_ "github.com/gogo/protobuf/sortkeys"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
 	_ "github.com/mikefarah/yq/v4"
 	_ "github.com/vmware-archive/yaml-patch/cmd/yaml-patch"
 	_ "k8s.io/code-generator"

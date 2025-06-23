0c0f6844049c42f1c6ef1f544436d31f4e262884
Linoy Hadad <lhadad@redhat.com>
2025-06-23 09:17:20 +0300

    test

--- monitoring/install.go
+++ monitoring/install.go
@@ -11,6 +11,7 @@ const (
 	GroupName = "monitoring.openshift.io"
 )
 
+// test
 var (
 	schemeBuilder = runtime.NewSchemeBuilder(monitoringv1.Install)
 	// Install is a function which adds every version of this group to a scheme

677781c5baa377908f5b86e3fc9b92fb8a57b0ab
Sandhya Dasu <sdasu@redhat.com>
2025-07-29 14:38:43 -0400

    Promote GCPClusterHostedDNSInstall to Default
    This promotes GCP Cluster Hosted DNS feature from techpreview to
    available by default.

--- features/features.go
+++ features/features.go
@@ -840,7 +840,7 @@ var (
 						contactPerson("barbacbd").
 						productScope(ocpSpecific).
 						enhancementPR("https://github.com/openshift/enhancements/pull/1468").
-						enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
+						enableIn(configv1.Default, configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
 						mustRegister()
 
 	FeatureGateAWSClusterHostedDNSInstall = newFeatureGate("AWSClusterHostedDNSInstall").

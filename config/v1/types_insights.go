25581c494bfb65ed56552da7fea6160a37159062
Ondrej Pokorny <opokorny@redhat.com>
2025-09-11 15:11:49 +0200

    feat: address review comments
    Signed-off-by: Ondrej Pokorny <opokorny@redhat.com>

--- config/v1/types_insights.go
+++ config/v1/types_insights.go
@@ -10,8 +10,7 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 //
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:path=insightsdatagathers,scope=Cluster
-// +kubebuilder:subresource:status
-// +openshift:api-approved.openshift.io=https://github.com/openshift/api/pull/2195
+// +openshift:api-approved.openshift.io=https://github.com/openshift/api/pull/2448
 // +openshift:file-pattern=cvoRunLevel=0000_10,operatorName=config-operator,operatorOrdering=01
 // +openshift:enable:FeatureGate=InsightsConfig
 //
@@ -26,20 +25,14 @@ type InsightsDataGather struct {
 	// spec holds user settable values for configuration
 	// +required
 	Spec InsightsDataGatherSpec `json:"spec,omitempty,omitzero"`
-	// status holds observed values from the cluster. They may not be overridden.
-	// +optional
-	Status InsightsDataGatherStatus `json:"status,omitempty,omitzero"`
 }
 
 // InsightsDataGatherSpec contains the configuration for the data gathering.
 type InsightsDataGatherSpec struct {
-	// gatherConfig is an optional spec attribute that includes all the configuration options related to gathering of the Insights data and its uploading to the ingress.
+	// gatherConfig is a required spec attribute that includes all the configuration options related to gathering of the Insights data and its uploading to the ingress.
 	// +required
 	GatherConfig GatherConfig `json:"gatherConfig,omitempty,omitzero"`
 }
-
-// +kubebuilder:validation:MinProperties=1
-type InsightsDataGatherStatus struct{}
 
 // gatherConfig provides data gathering configuration options.
 type GatherConfig struct {
@@ -65,17 +58,20 @@ type GatherConfig struct {
 }
 
 // +kubebuilder:validation:XValidation:rule="has(self.mode) && self.mode == 'Custom' ?  has(self.custom) : !has(self.custom)",message="custom is required when mode is Custom, and forbidden otherwise"
+// +union
 type Gatherers struct {
 	// mode is a required field that specifies the mode for gatherers. Allowed values are All, None, and Custom.
 	// When set to All, all gatherers wil run and gather data.
 	// When set to None, all gatherers will be disabled and no data will be gathered.
 	// When set to Custom, the custom configuration from the custom field will be applied.
+	// +unionDiscriminator
 	// +required
 	Mode GatheringMode `json:"mode,omitempty"`
 	// custom provides gathering configuration.
 	// It is required when mode is Custom, and forbidden otherwise.
 	// Custom configuration allows user to disable only a subset of gatherers.
 	// Gatherers that are not explicitly disabled in custom configuration will run.
+	// +unionMember
 	// +optional
 	Custom Custom `json:"custom,omitempty,omitzero"`
 }
@@ -124,15 +120,18 @@ const (
 // If the type is set to PersistentVolume, then the PersistentVolume must be defined.
 // If the type is set to Ephemeral, then the PersistentVolume must not be defined.
 // +kubebuilder:validation:XValidation:rule="has(self.type) && self.type == 'PersistentVolume' ?  has(self.persistentVolume) : !has(self.persistentVolume)",message="persistentVolume is required when type is PersistentVolume, and forbidden otherwise"
+// +union
 type Storage struct {
 	// type is a required field that specifies the type of storage that will be used to store the Insights data archive.
 	// Valid values are "PersistentVolume" and "Ephemeral".
 	// When set to Ephemeral, the Insights data archive is stored in the ephemeral storage of the gathering job.
 	// When set to PersistentVolume, the Insights data archive is stored in the PersistentVolume that is defined by the persistentVolume field.
+	// +unionDiscriminator
 	// +required
 	Type StorageType `json:"type,omitempty"`
 	// persistentVolume is an optional field that specifies the PersistentVolume that will be used to store the Insights data archive.
 	// The PersistentVolume must be created in the openshift-insights namespace.
+	// +unionMember
 	// +optional
 	PersistentVolume PersistentVolumeConfig `json:"persistentVolume,omitempty,omitzero"`
 }
@@ -167,7 +166,8 @@ type PersistentVolumeConfig struct {
 
 // persistentVolumeClaimReference is a reference to a PersistentVolumeClaim.
 type PersistentVolumeClaimReference struct {
-	// name is a string that follows the DNS1123 subdomain format.
+	// name is the name of the PersistentVolumeClaim that will be used to store the Insights data archive.
+	// It is a string that follows the DNS1123 subdomain format.
 	// It must be at most 253 characters in length, and must consist only of lower case alphanumeric characters, '-' and '.', and must start and end with an alphanumeric character.
 	// +kubebuilder:validation:XValidation:rule="!format.dns1123Subdomain().validate(self).hasValue()",message="a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character."
 	// +kubebuilder:validation:MinLength=1
@@ -178,7 +178,7 @@ type PersistentVolumeClaimReference struct {
 
 // gathererConfig allows to configure specific gatherers
 type GathererConfig struct {
-	// name is the required name of a specific gatherer
+	// name is the required name of a specific gatherer.
 	// It may not exceed 256 characters.
 	// The format for a gatherer name is: {gatherer}/{function} where the function is optional.
 	// Gatherer consists of a lowercase letters only that may include underscores (_).

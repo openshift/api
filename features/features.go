682edbd4e78449bd893f643a2b5cf214815b4a2c
Pablo Fontanilla <pfontani@redhat.com>
2025-09-19 19:01:24 +0200

    Promote DualReplica to default

--- features/features.go
+++ features/features.go
@@ -742,7 +742,7 @@ var (
 				contactPerson("jaypoulz").
 				productScope(ocpSpecific).
 				enhancementPR("https://github.com/openshift/enhancements/pull/1675").
-				enable(inTechPreviewNoUpgrade(), inDevPreviewNoUpgrade()).
+				enable(inDefault(), inTechPreviewNoUpgrade(), inDevPreviewNoUpgrade()).
 				mustRegister()
 
 	FeatureShortCertRotation = newFeatureGate("ShortCertRotation").

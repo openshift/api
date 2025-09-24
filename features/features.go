cb33f9f688df42f4161fdec298231ff1118d1202
Neeraj Krishna Gopalakrishna <ngopalak@redhat.com>
2025-09-24 08:40:26 +0530

    Remove NodeSwap Flag from OpenShift API

--- features/features.go
+++ features/features.go
@@ -92,15 +92,6 @@ var (
 					enableIn(configv1.Default, configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
 					mustRegister()
 
-	// NodeSwap is now GA and locked to true in Kubernetes 1.34+
-	// Must be enabled to align with upstream Kubernetes behavior.
-	FeatureGateNodeSwap = newFeatureGate("NodeSwap").
-				reportProblemsToJiraComponent("node").
-				contactPerson("haircommander").
-				productScope(kubernetes).
-				enhancementPR("https://github.com/kubernetes/enhancements/issues/2400").
-				enableIn(configv1.Default, configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
-				mustRegister()
 
 	FeatureGateDynamicResourceAllocation = newFeatureGate("DynamicResourceAllocation").
 						reportProblemsToJiraComponent("scheduling").

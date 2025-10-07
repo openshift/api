817e971bc174971e4ac984fd1be224ac8de49f97
Thomas Jungblut <tjungblu@redhat.com>
2025-10-07 16:55:35 +0200

    CNTRLPLANE-1576: add event-ttl configuration to kube-apiserver
    This adds a minute based configuration to configure the event ttl setting in kube-apiserver.
    Default will stay 3h, as currently defined in KAS-O.
    
    Signed-off-by: Thomas Jungblut <tjungblu@redhat.com>

--- openapi/generated_openapi/zz_generated.openapi.go
+++ openapi/generated_openapi/zz_generated.openapi.go
@@ -53284,6 +53284,13 @@ func schema_openshift_api_operator_v1_KubeAPIServerSpec(ref common.ReferenceCall
 							Format:      "int32",
 						},
 					},
+					"eventTTLMinutes": {
+						SchemaProps: spec.SchemaProps{
+							Description: "eventTTLMinutes specifies the amount of time that the events are stored before being deleted. The TTL is allowed between 5 minutes minimum up to a maximum of 180 minutes (3 hours).\n\nLowering this value will reduce the storage required in etcd. Note that this setting will only apply to new events being created and will not update existing events.\n\nWhen omitted this means no opinion, and the platform is left to choose a reasonable default, which is subject to change over time. The current default value is 3h (180 minutes).",
+							Type:        []string{"integer"},
+							Format:      "int32",
+						},
+					},
 				},
 				Required: []string{"managementState", "forceRedeploymentReason"},
 			},

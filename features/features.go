0a87dc54a866239ba3e688b1562dece54444e0e8
Riccardo Ravaioli <rravaiol@redhat.com>
2025-11-17 11:45:11 +0100

    Add API support and FeatureGate for OVN-Kubernetes no-overlay mode
    This commit introduces API changes to support OVN-Kubernetes no-overlay mode for the default network,
    where the cluster default network operates without overlay encapsulation (Geneve).
    
    API changes (operator.openshift.io/v1):
    - Added defaultNetworkTransport field to OVNKubernetesConfig (NoOverlay or Geneve)
    - Added defaultNetworkNoOverlayOptions with outboundSNAT and routing configuration
    - Added bgpManagedConfig for BGP configuration when routing is managed
    - All no-overlay fields are immutable after cluster creation
    
    Added feature gate (TechPreviewNoUpgrade) for NoOverlayMode within the Networking/ovn-kubernetes component.
    
    Ran these commands to update autogenerated source files:
    $ hack/update-codegen-crds.sh
    $ hack/update-swagger-docs.sh
    $ hack/update-payload-featuregates.sh
    
    Openshift enhancement for this feature: https://github.com/openshift/enhancements/pull/1859
    
    Signed-off-by: Riccardo Ravaioli <rravaiol@redhat.com>

--- features/features.go
+++ features/features.go
@@ -187,6 +187,14 @@ var (
 					enableIn(configv1.Default, configv1.OKD, configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
 					mustRegister()
 
+	FeatureGateNoOverlayMode = newFeatureGate("NoOverlayMode").
+					reportProblemsToJiraComponent("Networking/ovn-kubernetes").
+					contactPerson("pliurh").
+					productScope(ocpSpecific).
+					enhancementPR("https://github.com/openshift/enhancements/pull/1859").
+					enableIn(configv1.TechPreviewNoUpgrade).
+					mustRegister()
+
 	FeatureGateAdditionalRoutingCapabilities = newFeatureGate("AdditionalRoutingCapabilities").
 							reportProblemsToJiraComponent("Networking/cluster-network-operator").
 							contactPerson("jcaamano").

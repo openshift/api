84a80be0fc7aa712c1459b5b5d5f9ad519bd8528
Forrest Babcock <36795216+neisw@users.noreply.github.com>
2025-12-10 16:21:30 -0500

    Revert "MULTIARCH-5190: Promote ImageStreamImportMode to default"

--- features/features.go
+++ features/features.go
@@ -588,7 +588,7 @@ var (
 						contactPerson("psundara").
 						productScope(ocpSpecific).
 						enhancementPR(legacyFeatureGateWithoutEnhancement).
-						enableIn(configv1.Default, configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
+						enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
 						mustRegister()
 
 	FeatureGateUserNamespacesSupport = newFeatureGate("UserNamespacesSupport").

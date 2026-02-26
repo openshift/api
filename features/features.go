3757b1d4c6b83a7f3fe85739beae8cd9ed258577
Joseph Callen <jcallen@redhat.com>
2026-02-26 13:34:35 -0500

    vsphere vm-host zonal ga
    This commit is to add the vSphere vm-host zonal feature to default (GA).

--- features/features.go
+++ features/features.go
@@ -183,7 +183,7 @@ var (
 						contactPerson("jcpowermac").
 						productScope(ocpSpecific).
 						enhancementPR("https://github.com/openshift/enhancements/pull/1677").
-						enable(inTechPreviewNoUpgrade(), inDevPreviewNoUpgrade()).
+						enable(inDefault(), inOKD(), inTechPreviewNoUpgrade(), inDevPreviewNoUpgrade()).
 						mustRegister()
 
 	FeatureGateVSphereMultiDisk = newFeatureGate("VSphereMultiDisk").

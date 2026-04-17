3ea7135cc4bd4e65cbec06c78457dc1f9610a178
Thomas Jungblut <tjungblu@redhat.com>
2026-04-17 15:12:35 +0200

    CNTRLPLANE-2796: promote the event-ttl feature
    Enables the event-ttl feature by default.
    This is a backport of openshift#2722 to 4.21.
    
    Signed-off-by: Thomas Jungblut <tjungblu@redhat.com>

--- features/features.go
+++ features/features.go
@@ -659,12 +659,12 @@ var (
 						mustRegister()
 
 	FeatureGateKMSEncryption = newFeatureGate("KMSEncryption").
-		reportProblemsToJiraComponent("kube-apiserver").
-		contactPerson("ardaguclu").
-		productScope(ocpSpecific).
-		enhancementPR("https://github.com/openshift/enhancements/pull/1900").
-		enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
-		mustRegister()
+					reportProblemsToJiraComponent("kube-apiserver").
+					contactPerson("ardaguclu").
+					productScope(ocpSpecific).
+					enhancementPR("https://github.com/openshift/enhancements/pull/1900").
+					enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
+					mustRegister()
 
 	FeatureGateHighlyAvailableArbiter = newFeatureGate("HighlyAvailableArbiter").
 						reportProblemsToJiraComponent("Two Node with Arbiter").
@@ -897,7 +897,7 @@ var (
 			contactPerson("tjungblu").
 			productScope(ocpSpecific).
 			enhancementPR("https://github.com/openshift/enhancements/pull/1857").
-			enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
+			enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade, configv1.Default).
 			mustRegister()
 
 	FeatureGateMutableCSINodeAllocatableCount = newFeatureGate("MutableCSINodeAllocatableCount").

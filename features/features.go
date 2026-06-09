559b4c50d9291ad7fc1df3a2a72be539d326e621
Joe Lanford <joe.lanford@gmail.com>
2026-06-09 11:59:37 -0400

    Promote TLSAdherence feature gate to GA

--- features/features.go
+++ features/features.go
@@ -994,7 +994,7 @@ var (
 				contactPerson("joelanford").
 				productScope(ocpSpecific).
 				enhancementPR("https://github.com/openshift/enhancements/pull/1910").
-				enable(inOKD(), inDevPreviewNoUpgrade(), inTechPreviewNoUpgrade()).
+				enable(inDefault(), inOKD(), inTechPreviewNoUpgrade(), inDevPreviewNoUpgrade()).
 				mustRegister()
 
 	FeatureGateConfidentialCluster = newFeatureGate("ConfidentialCluster").

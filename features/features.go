c22530086118b3c41fcdc0048572b31ea8a4dbf4
Neil Girard <45597105+vr4manta@users.noreply.github.com>
2026-06-10 14:30:56 -0400

    Revert "TRT-2701: Revert "SPLAT-2793: Promoted VSphereMultiVCenterDay2 feature gate to TP""
    (cherry picked from commit 1bf28d79f9810d29d8c1f3afe3596d246e6cd64e)

--- features/features.go
+++ features/features.go
@@ -788,7 +788,7 @@ var (
 						contactPerson("vr4manta").
 						productScope(ocpSpecific).
 						enhancementPR("https://github.com/openshift/enhancements/pull/1961").
-						enableIn(configv1.DevPreviewNoUpgrade).
+						enableIn(configv1.DevPreviewNoUpgrade, configv1.TechPreviewNoUpgrade).
 						mustRegister()
 
 	FeatureGatePreconfiguredUDNAddresses = newFeatureGate("PreconfiguredUDNAddresses").

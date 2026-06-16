546fe417549f738d6d17e5a9e25e31f6698009ce
Ben Perry <bhperry94@gmail.com>
2026-06-16 10:11:33 -0500

    EtcdBackup and EtcdBackupPolicy apis

--- hack/update-payload-crds.sh
+++ hack/update-payload-crds.sh
@@ -17,7 +17,7 @@ crd_globs="\
     operator/v1/zz_generated.crd-manifests/0000_20_kube-apiserver_01_kubeapiservers*.crd.yaml
     operator/v1/zz_generated.crd-manifests/0000_12_etcd_01_etcds*.crd.yaml
     operator/v1alpha1/zz_generated.crd-manifests/0000_10_etcd_01_etcdbackups*.crd.yaml
-    config/v1alpha1/zz_generated.crd-manifests/0000_10_config-operator_01_backups*.crd.yaml
+    operator/v1alpha1/zz_generated.crd-manifests/0000_10_etcd_01_etcdbackuppolicies*.crd.yaml
     operator/v1/zz_generated.crd-manifests/0000_25_kube-scheduler_01_kubeschedulers*.crd.yaml
     operator/v1/zz_generated.crd-manifests/0000_25_kube-controller-manager_01_kubecontrollermanagers*.crd.yaml
     config/v1/zz_generated.crd-manifests/0000_10_openshift-controller-manager_01_builds*.crd.yaml

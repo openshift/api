089a62adca7c9f3c4ebc6731e406fb74c013604d
Ondra Kupka <okupka@redhat.com>
2026-09-10 15:09:06 +0200

    Add AuthenticationComponentProxyExternalOIDC feature gate
    This feature gate gates component-scoped proxy support for the External
    OIDC authentication stack.

--- features/features.go
+++ features/features.go
@@ -344,6 +344,14 @@ var (
 						enable(inClusterProfile(SelfManaged), inTechPreviewNoUpgrade(), inDevPreviewNoUpgrade()).
 						mustRegister()
 
+	FeatureGateAuthenticationComponentProxyExternalOIDC = newFeatureGate("AuthenticationComponentProxyExternalOIDC").
+								reportProblemsToJiraComponent("authentication").
+								contactPerson("tchap").
+								productScope(ocpSpecific).
+								enhancementPR("https://github.com/openshift/enhancements/pull/2097").
+								enable(inTechPreviewNoUpgrade(), inDevPreviewNoUpgrade()).
+								mustRegister()
+
 	FeatureGateExternalOIDCWithAdditionalClaimMappings = newFeatureGate("ExternalOIDCWithUIDAndExtraClaimMappings").
 								reportProblemsToJiraComponent("authentication").
 								contactPerson("bpalmer").

4a92b173af2867d38f03967c95869e8e066719e2
Jonathan Dobson <jdobson@redhat.com>
2026-09-18 09:14:20 -0600

    STOR-2859: add comment about promoting both feature gates together

--- features/features.go
+++ features/features.go
@@ -1075,6 +1075,8 @@ var (
 					enhancementPR("https://github.com/kubernetes/enhancements/issues/5055").
 					mustRegister()
 
+	// The ControllerManagerConfig and DisableForceDetachOnTimeout feature gates must be promoted together, because
+	// DisableForceDetachOnTimeout enables the only field in ControllerManagerSpec and the spec can not be empty.
 	FeatureGateControllerManagerConfig = newFeatureGate("ControllerManagerConfig").
 						reportProblemsToJiraComponent("kube-controller-manager").
 						contactPerson("jdobson").
@@ -1082,7 +1084,6 @@ var (
 						enhancementPR("https://github.com/openshift/enhancements/pull/2095").
 						enable(inDevPreviewNoUpgrade()).
 						mustRegister()
-
 	FeatureGateDisableForceDetachOnTimeout = newFeatureGate("DisableForceDetachOnTimeout").
 						reportProblemsToJiraComponent("Storage / Kubernetes").
 						contactPerson("jdobson").
